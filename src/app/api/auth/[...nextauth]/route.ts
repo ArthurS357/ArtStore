b0276e5d194ce30e2aeaf5e36e8d2e6728b37a81
TR <arthursabino5342@gmail.com>
2025-10-04 22:44:21 -0300

    refactor: Centraliza a configuração do NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcryptjs from "bcryptjs";
-
-const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null;
-        const user = await prisma.user.findUnique({ where: { email: credentials.email } });
-        if (!user || !user.password) return null;
-        const passwordMatch = await bcryptjs.compare(credentials.password, user.password);
-        if (!passwordMatch) return null;
-        return user;
-      }
-    })
-  ],
-  pages: { signIn: '/login' },
-  callbacks: {
-    session({ session, token }) {
-      if (token && session.user) { (session.user as any).id = token.sub; }
-      return session;
-    },
-  },
-  session: { strategy: "jwt" },
-  secret: process.env.NEXTAUTH_SECRET,
-  debug: process.env.NODE_ENV === "development",
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth"; 
 
 const handler = NextAuth(authOptions);
 
