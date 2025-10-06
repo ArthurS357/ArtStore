b88cbce2f4268c39ea74731e9090a6e41629a7d0
Arthur S. <114530733+ArthurS357@users.noreply.github.com>
2025-10-06 16:02:43 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,10 @@
 export { default } from "next-auth/middleware";
 
+// Apenas as rotas listadas abaixo serão protegidas e exigirão login.
 export const config = {
   matcher: [
-    /*
-     * Corresponde a todos os caminhos de solicitação, exceto aqueles que começam com:
-     * - api (rotas de API)
-     * - _next/static (arquivos estáticos)
-     * - _next/image (arquivos de otimização de imagem)
-     * - images (imagens públicas)
-     * - favicon.ico (arquivo de favicon)
-     * - login (página de login)
-     * - registro (página de registro)
-     */
-    '/((?!api/|_next/static|_next/image|images|favicon.ico|login|registro).*)',
+    '/perfil/:path*',   // Protege a página de perfil
+    '/cart/:path*',     // Protege a página do carrinho
+    '/product/:path*',  // Protege todas as páginas de detalhes de produto
   ],
 };
