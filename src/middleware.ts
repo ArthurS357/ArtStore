619ff24622eae888d4e81a4b5a8e65601db2a617
Arthur S. <114530733+ArthurS357@users.noreply.github.com>
2025-10-06 11:44:57 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,16 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/((?!api/| _next/static| _next/image|favicon.ico|images/|login|registro).*)",
+    /*
+     * Corresponde a todos os caminhos de solicitação, exceto aqueles que começam com:
+     * - api (rotas de API)
+     * - _next/static (arquivos estáticos)
+     * - _next/image (arquivos de otimização de imagem)
+     * - images (imagens públicas)
+     * - favicon.ico (arquivo de favicon)
+     * - login (página de login)
+     * - registro (página de registro)
+     */
+    '/((?!api/|_next/static|_next/image|images|favicon.ico|login|registro).*)',
   ],
 };
